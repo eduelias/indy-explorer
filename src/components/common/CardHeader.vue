c1f1eb4856e287e19c118f2d87f24eeef46c93db
eduelias <du7@msn.com>
2020-05-01 12:48:27 +0200

    feat(txraw): adding tx raw on click
    Signed-off-by: eduelias <du7@msn.com>

---
+++ src/components/common/CardHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="`color: ${color}`" :class="`row q-ma-sm tx_header`">
+    {{ type }} - {{ formatDate(date) }}<q-space />
+    <div class="float-right cursor-pointer text-h5 tx_number" @click="click">#{{ seqNo }}</div>
+  </div>
+</template>
+
+<script>
+import { date } from 'quasar';
+
+export default {
+  props: {
+    color: String,
+    type: String,
+    date: Number,
+    seqNo: Number,
+  },
+  methods: {
+    formatDate: function(inputDate) {
+      return date.formatDate(new Date(inputDate * 1000), 'MMMM Do YYYY, HH:mm:ss (Z)');
+    },
+    click: function() {
+      this.$emit('click', {});
+    },
+  },
+};
+</script>
